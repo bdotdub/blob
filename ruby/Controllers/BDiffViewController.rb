d3965ed399b17076f1d3f758b62c7227f9c5f4d6
Benny Wong <bdotdub@gmail.comg>
2009-10-13 00:16:59 -0400

    added some git file path and revision stuff. loading diff function. adding css colors

--- ruby/Controllers/BDiffViewController.rb
+++ ruby/Controllers/BDiffViewController.rb
@@ -20,16 +20,57 @@ class BDiffViewController < NSObject
   end
 
   def previous_revision(sender)
-    script_object = @webView.windowScriptObject()
-    puts script_object.inspect
-    args = ["hello there!"]
-    script_object.callWebScriptMethod("replaceContainerWithHTML", withArguments:args)
+    load_file_diffs(@git_file)
   end
 
   def forward_revision(sender)
   end
-  
-  def load_file_diffs(git_file)
+
+  def load_file_diffs(git_file, first_revision=nil, second_revision=nil)
+  @git_file = git_file
+    first_revision = git_file.current_revision    if first_revision.nil?
+    second_revision = git_file.previous_revision  if second_revision.nil?
     
+    file_diff = `#{Git.path} diff -U10000 #{first_revision}..#{second_revision} -- #{git_file.path}`
+    file_lines = file_diff.split("\n")
+    remove_diff_header file_lines
+
+    diff_output = '<pre>'
+    file_lines.each do |line|
+      line_output = ''
+      if line =~ /^\+/
+        line_output << "<div class='green'>"
+      elsif line =~ /^-/
+        line_output << "<div class='red'>"
+      else
+        line_output << "<div>"
+      end
+
+      line_output << line # entities(line)
+      line_output << "</div>"
+
+      diff_output << line_output
+    end
+    diff_output << '</pre>'
+    replace_container_text diff_output
+  end
+
+  def remove_diff_header(lines)
+    index_of_last_diff_line = -1
+    lines.each_with_index do |line, index|
+      if line =~ /^@@/
+        index_of_last_diff_line = index
+        break
+      end
+    end
+
+    raise "Could not find last line of diff" if index_of_last_diff_line == -1
+    (index_of_last_diff_line + 1).times do lines.shift end
+  end
+  
+  def replace_container_text(html)
+    script_object = @webView.windowScriptObject()
+    args = [html]
+    script_object.callWebScriptMethod("replaceContainerWithHTML", withArguments:args)
   end
 end
--- ruby/lib/Git/File.rb
+++ ruby/lib/Git/File.rb
@@ -7,12 +7,14 @@
 class Git
   class File
     attr_accessor :revisions
+    attr_accessor :path
 
     def initialize(filename)
       Dir.chdir(::File.expand_path(::File.dirname(filename)))
       @revisions = `#{Git.path} rev-list HEAD -- #{filename}`.chomp.split("\n")
 
       raise Git::FileUntracked if $? != 0
+      @path = filename
       @current_revision_index = 0
     end
 
@@ -21,11 +23,12 @@ class Git
     end
 
     def previous_revision
+      revision_hash = nil
       if @current_revision_index <= (@revisions.length - 2)
-        @revisions[@current_revision_index + 1]
+        revision_hash = @revisions[@current_revision_index + 1]
       end
 
-      nil
+      revision_hash
     end
   end
 end
