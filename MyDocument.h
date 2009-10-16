97e52c877fe26efcb85cc843e5c5f29e6c6435ea
Benny Wong <bdotdub@gmail.com>
2009-10-15 23:08:54 -0400

    fresh document based cocoa app

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  blob
+//
+//  Created by Benny Wong on 10/15/09.
+//  Copyright __MyCompanyName__ 2009 . All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
